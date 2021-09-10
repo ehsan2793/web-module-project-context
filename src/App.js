20f80f7027cf128c748da1488dfc6355a6772f17
ehsan2793 <ehsan2793@gmail.com>
2021-09-09 21:58:59 -0700

    compeleted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import data from './data';
 //context 
 import { ProductContext } from './contexts/productContext';
+import { CartContext } from './contexts/cartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -19,19 +20,22 @@ function App() {
 
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{ products, addItem }} >
-				<Navigation cart={cart} />
+
+			<CartContext.Provider value={{ cart }}>
+				<Navigation />
 
 				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
-
+				<ProductContext.Provider value={{ products, addItem }} >
+					<Route exact path="/">
+						<Products />
+					</Route>
+				</ProductContext.Provider>
 				<Route path="/cart">
-					<ShoppingCart cart={cart} />
+					<ShoppingCart />
 				</Route>
+			</CartContext.Provider>
 
-			</ProductContext.Provider>
+
 		</div >
 	)
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -4,16 +4,16 @@ import { ProductContext } from '../contexts/productContext';
 // Components
 import Product from './Product';
 
-const Products = props => {
-	const value = useContext(ProductContext)
-	console.log(value);
+const Products = () => {
+	const { products, addItem } = useContext(ProductContext)
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 			))}
 		</div>
